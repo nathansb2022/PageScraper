ef643258dc071daf3c95176c6f362211c53865c4
Nathansb2022 <Nathansb2010@gmail.com>
2023-09-30 03:10:01 -0400

    add hacker news flavor

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # PageScraper
-Python script to gather the text of a html web page and display it in the console. Addtitionally, has the ability to speak the provided text. Tested on Debian Distribution.
+
+Python script to gather the text of a html web page and display it in the console. Addtitionally, has the ability to speak the provided text. Tested on Debian Distribution. There is "The Hacker News" version in theHackerNews folder.
 
 # Terminal Output
 
@@ -14,6 +15,10 @@ Add URL as argument and y/n for verbal response
 python3 pagescraper.py 10.10.10.10 n
 ```
 Or without
+```bash
+python3 pagescraperTHN.py 10.10.10.10 y | tee thn_9_20_23.txt
+```
+Or pipe it to tee command to store in a text file
 ```bash
 python3 pagescraper.py
 ```
---
+++ theHackerNews/pagescraperTHN.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+#Time to page scrape thhackernews.com
+#Works to gather text from the html of the URL and displays the results
+#USE: python3 pagescraper.py
+#     python3 pagescraper.py https://www.google.com
+#     python3 pagescraper.py https://www.google.com y
+#INSTALL
+#sudo pip3 install espeak rainbowtext PyAudio
+import requests, sys, pyttsx3, rainbowtext, os, time
+from colorama import Fore
+from bs4 import BeautifulSoup
+# Do you want a verbal response. Can be passed as a cmd line argument
+def audioOrNot(site):
+	if not len(sys.argv) > 2:
+		i = input(Fore.GREEN + 'Do you want a verbal response? (y/n) ')
+		if i == 'y':
+			startIt(site)
+		elif i == 'n':
+			scrape(site)
+		else:
+			print(Fore.RED + 'Error occured')
+	elif sys.argv[2] == 'y':
+		startIt(site)
+	elif sys.argv[2] == 'n':
+		scrape(site)
+	else:
+		print(Fore.RED + 'Error occured')
+# Check if the url was passed as a cmd line argument
+def checkURL():
+	if not len(sys.argv) > 1:
+		site = input(Fore.GREEN + "Please input your URL: ")
+		return site
+	else:
+		site = sys.argv[1]
+		return site
+# Pull the text from the html and print out
+def scrape(site):
+	r = requests.get(site)
+	html = r.text
+	soup = BeautifulSoup(html, "html.parser")
+	lines = soup.body.get_text().strip()
+	with open("filename","xt") as fd:
+		for line in lines:
+			fd.write(line)
+		fd.close()
+	with open("filename", "r+") as fd:
+		fileLines = fd.readlines()
+		fd.seek(0)
+		fd.truncate()
+		fd.writelines(fileLines[110:-270])
+		fd.close()
+	with open('filename', 'r') as fd:
+		what_say = fd.read()
+		fd.close()
+	outtie = rainbowtext.text(what_say)
+	print(outtie)
+	return what_say
+# Speak the contents gathered by scrape()
+def startIt(site):
+
+	engine = pyttsx3.init()
+	engine.setProperty('rate', 135)
+	engine.say(scrape(site))
+	engine.runAndWait()
+if __name__ == "__main__":
+	site = checkURL()
+	audioOrNot(site)
+	os.remove("filename")
